979c02cd54c6e24734e56bdb2734133538d2c3bc
DanielStancioi <daniel.stancioi00@e-uvt.ro>
2021-10-13 11:10:05 +0300

    add a new book

--- sabloaneProiectareBook/src/sabloaneProiectareBook/Main.java
+++ sabloaneProiectareBook/src/sabloaneProiectareBook/Main.java
@@ -13,6 +13,10 @@ public class Main {
 		discoTitanic.addNewTable("Table 1");
 		discoTitanic.addNewParagraph("Paragraph 5");
 		discoTitanic.print();
+		Book discoTitanic1 = new Book("Disco Titanic");
+		discoTitanic1.addNewParagraph("Paragraph 1");
+		discoTitanic1.addNewParagraph("Paragraph 2");
+		discoTitanic1.print();
 	}
 
 }
